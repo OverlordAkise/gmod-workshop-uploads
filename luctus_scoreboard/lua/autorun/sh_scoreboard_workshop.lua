953c3fc47861e412aa43455ce156457e63eb4ecb
OverlordAkise <OverlordAkise@gmail.com>
2024-02-16 16:19:37 +0100

    Added luctus_scoreboard

---
+++ luctus_scoreboard/lua/autorun/sh_scoreboard_workshop.lua
@@ -0,0 +1,3 @@
+
+LUCTUS_SCOREBOARD_CONVAR_URL = CreateConVar("luctus_scoreboard_url", "https://luctus.at", {FCVAR_ARCHIVE,FCVAR_REPLICATED}, "scoreboard url")
+LUCTUS_SCOREBOARD_CONVAR_TITLE = CreateConVar("luctus_scoreboard_title", "DarkRP", {FCVAR_ARCHIVE,FCVAR_REPLICATED}, "scoreboard title")
